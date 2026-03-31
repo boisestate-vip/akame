76e84687f340f2b6484da33f0d4ff186451453b8
lex watts <lexwatts@u.boisestate.edu>
2026-03-30 20:11:37 -0600

    central controller ready for testing

--- src/py_pubsub/setup.py
+++ src/py_pubsub/setup.py
@@ -1,25 +1,29 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-package_name = 'central_controller'
+package_name = 'py_pubsub'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-         ['resource/' + package_name]),
+            ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Lex Watts',
+    maintainer='ladyluna_77',
     maintainer_email='lexwatts@u.boisestate.edu',
-    description='Central controller node for bender',
-    license='MIT',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
     entry_points={
         'console_scripts': [
-            'central_controller_node = central_controller.central_controller_node:main',
         ],
     },
 )
